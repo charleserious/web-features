9c7574039de38bfd321b0c186baebfe66680f9b1
charleserious <wujinjing25@gmail.com>
2018-03-15 10:20:20 +0800

    add Promise.wrap

--- Promise/Scripts/promise.utility.js
+++ Promise/Scripts/promise.utility.js
@@ -52,5 +52,25 @@
             );
         };
     }
+
+    if(!Promise.wrap) {
+        Promise.wrap = function (fn) {
+          return function () {
+            var args = [].slice.call( arguments );
+            return new Promise( function (resolve, reject) {
+                fn.apply(
+                    null,
+                    args.concat( function (err, v) {
+                        if (err) {
+                            reject( err );
+                        } else {
+                            resolve( v );
+                        }
+                    } )
+                );
+            } );
+          };
+        };
+    }
 })();
 
