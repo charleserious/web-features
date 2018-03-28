f278f0a46f93c02d0ac1199b921b6e0da982a260
charleserious <wujinjing25@gmail.com>
2018-03-28 15:49:07 +0800

    mv Iterate to Generator, add Generator Delegation examination

---
+++ Generators/Scripts/13.GeneratorDelegation.js
@@ -0,0 +1,27 @@
+function * foo () {
+	console.log('`*foo ()` starting');
+	yield 3;
+	yield 4;
+	console.log('`*foo ()` finished');
+}
+
+function * bar () {
+	yield 1;
+	yield 2;
+	yield * foo();	// `yield` delegation!
+	yield 5;
+}
+
+var it = bar();
+var value;
+
+value = it.next().value;	// 1
+console.log(value);
+value = it.next().value;	// 2
+console.log(value);
+value = it.next().value;	// `*foo ()` starting // 3
+console.log(value);
+value = it.next().value;	// 4
+console.log(value);
+value = it.next().value;	// `*foo ()` finished // 5
+console.log(value);
